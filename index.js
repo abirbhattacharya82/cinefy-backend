4937387ec3920ea93cf5fa8127a553be49640f76
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2023-04-04 09:35:47 +0530

    Login

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 app.listen(port);
 const mongoURI = process.env.url;
 const MongoClient = require('mongodb').MongoClient;
+const jwt = require('jsonwebtoken');
 
 MongoClient.connect(mongoURI, { useUnifiedTopology: true })
     .then(client => {
@@ -49,7 +50,7 @@ MongoClient.connect(mongoURI, { useUnifiedTopology: true })
 MongoClient.connect(mongoURI, { useUnifiedTopology: true })
     .then(client => {
         const db = client.db('cinefy');
-        const userCollections = db.collection('users');
+        const userCollection = db.collection('users');
 
         app.post('/register', async (req, res) => {
             const username = req.query.username;
@@ -61,12 +62,40 @@ MongoClient.connect(mongoURI, { useUnifiedTopology: true })
                 "password": password
             }
             try {
-                const result = await userCollections.insertOne(obj);
+                const result = await userCollection.insertOne(obj);
                 console.log(result);
                 res.status(200).send({ "message": "success" })
             } catch (error) {
                 console.error(error);
                 res.status(404).send({ "message": "username already exists" });
+            }
+        });
+
+        app.get('/login', async (req, res) => {
+            const username = req.query.username;
+            const password = req.query.password;
+            const obj = {
+                "_id": {
+                    "username": username
+                },
+                "password": password
+            }
+            try {
+                const cursor = await userCollection.find();
+                const user = await cursor.toArray();
+                if(user.length!=0)
+                {
+                    const secret=process.env.secret;
+                    const payload={userID:user[0]._id.username};
+                    const option={expiresIn:'1h'};
+                    const token=jwt.sign(payload,secret,option);
+                    res.status(200).send({ "message": "successs", "username": user[0]._id.username, "token": token });
+                }
+                else
+                    res.status(404).send({ "message": "Error retrieving movies from database." });
+            } catch (error) {
+                console.error(error);
+                res.status(404).send({ "message": "Error retrieving movies from database." });
             }
         });
     }
